33705a9d7d6a01bb31bed2ddc7170c2bb7cc945b
Laziz <lazizo2004@gmail.com>
2023-07-30 18:54:52 +0500

    add JavaDoc for Validator

--- src/main/java/com/dev/socialPoll/service/validator/PollValidator.java
+++ src/main/java/com/dev/socialPoll/service/validator/PollValidator.java
@@ -2,7 +2,19 @@ package com.dev.socialPoll.service.validator;
 
 public interface PollValidator {
 
+    /**
+     * Checks if the poll name is valid.
+     *
+     * @param pollName the poll name to be checked.
+     * @return true if the poll name is valid, false otherwise.
+     */
     boolean checkPollName(String pollName);
 
+    /**
+     * Checks if the poll description is valid.
+     *
+     * @param pollDescription the poll description to be checked.
+     * @return true if the poll description is valid, false otherwise.
+     */
     boolean checkPollDescription(String pollDescription);
 }
--- src/main/java/com/dev/socialPoll/service/validator/impl/PollValidatorImpl.java
+++ src/main/java/com/dev/socialPoll/service/validator/impl/PollValidatorImpl.java
@@ -12,11 +12,13 @@ public class PollValidatorImpl implements PollValidator {
 
     @Override
     public boolean checkPollName(String pollName) {
+        // Validates the poll name length (3 to 30 characters).
         return pollName.length() >= 3 && pollName.length() <= 30;
     }
 
     @Override
     public boolean checkPollDescription(String pollDescription) {
+        // Validates the poll description by checking if it contains more than 2 words.
         return pollDescription.split(" ").length > 2;
     }
 }
