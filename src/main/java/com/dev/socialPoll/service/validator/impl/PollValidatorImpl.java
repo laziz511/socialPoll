90fb1e639eec7f4d1da38332f1b894c523babc0f
Laziz <lazizo2004@gmail.com>
2023-08-07 12:25:34 +0500

    update PollValidatorImpl

--- src/main/java/com/dev/socialPoll/service/validator/impl/PollValidatorImpl.java
+++ src/main/java/com/dev/socialPoll/service/validator/impl/PollValidatorImpl.java
@@ -13,12 +13,12 @@ public class PollValidatorImpl implements PollValidator {
     @Override
     public boolean checkPollName(String pollName) {
         // Validates the poll name length (3 to 30 characters).
-        return pollName.length() >= 3 && pollName.length() <= 30;
+        return pollName.length() >= 3 && pollName.length() <= 50;
     }
 
     @Override
     public boolean checkPollDescription(String pollDescription) {
         // Validates the poll description by checking if it contains more than 2 words.
-        return pollDescription.split(" ").length > 2;
+        return pollDescription.split(" ").length >= 2;
     }
 }
