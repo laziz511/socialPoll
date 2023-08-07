aa5d9f9f4c1129eb4189d5af41d971c9b9cb8dda
Laziz <lazizo2004@gmail.com>
2023-08-07 12:07:49 +0500

    update AdminDashboardServlet

--- src/main/java/com/dev/socialPoll/servlets/AdminDashboardServlet.java
+++ src/main/java/com/dev/socialPoll/servlets/AdminDashboardServlet.java
@@ -7,22 +7,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 
 @WebServlet("/admin-dashboard")
 public class AdminDashboardServlet extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger();
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
+        User user = (User) request.getSession().getAttribute("user");
 
         if (user != null && user.getRole() == UserRole.ADMIN) {
             request.getRequestDispatcher("html/admin/admin-dashboard.jsp").forward(request, response);
